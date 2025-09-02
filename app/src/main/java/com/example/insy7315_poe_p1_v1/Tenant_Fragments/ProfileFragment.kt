aff6e765686f0768085c8dc306fecc920e014ece
Jarryd K <ST10233515@vcconnect.edu.za>
2025-09-02 22:53:35 +0200

    Dark mode and settings
    Added a dark mode feature to the settings page, also added a settings page.
    Just need to sort out the colour scheme then all is well.

--- app/src/main/java/com/example/insy7315_poe_p1_v1/Tenant_Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/insy7315_poe_p1_v1/Tenant_Fragments/ProfileFragment.kt
@@ -1,11 +1,12 @@
 package com.example.insy7315_poe_p1_v1.Tenant_Fragments
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.insy7315_poe_p1_v1.R
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
@@ -14,6 +15,19 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tenant_fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.tenant_fragment_profile, container, false)
+
+        val btnSettings = view.findViewById<Button>(R.id.btnSettings)
+
+        btnSettings.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingsFragment())
+                .addToBackStack(null) // Lets the user press back to return here
+                .commit()
+
+            (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        return view
     }
 }
